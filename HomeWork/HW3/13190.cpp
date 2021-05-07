b36c0b0abd54b350ffe9cc7ca88429c57a91183c
ShaoFuLiu <zx22920416@gmail.com>
2021-05-08 04:56:49 +0800

    20210508

--- HomeWork/HW3/13190.cpp
+++ HomeWork/HW3/13190.cpp
@@ -1,17 +1,83 @@
-#include<iostream>
-#include<vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 struct node
 {
-	int v, col, row;
-	bool passed;
+	int value, col, row;
+	bool passed = false;
 	struct node *up;
 	struct node *down;
 	struct node *left;
 	struct node *right;
 }*root;
 
-//use doubled non circular linked list representation to store matirx
+
+class Tree
+{
+    public:
+		int *arr;
+		void Store_matrix(int value, int i, int j, int col);
+		void Build_Tree(int y, int x);
+        void add_Lnode(int value, int row, int col);
+		void add_Rnode(int value, int row, int col);
+		void add_Unode(int value, int row, int col);
+		void add_Dnode(int value, int row, int col);
+		int minPathSum();
+        void Print(int row, int col);
+        Tree()
+        {
+            root = NULL;
+        }
+};
+
+int main(void)
+{
+	Tree tree;
+	int row, col, value, x, y;
+	cin >> row >> col;
+	cin >> x >> y;
+	int mat[row][col];
+
+	for(int i = 0; i < row; i++)
+	{
+		for(int j = 0; j < col; j++)
+		{
+			cin >> value;
+			mat[i][j] = value;
+			tree.Store_matrix(value, i, j, col);
+		}
+	}
+	tree.Print(row, col);
+	// tree.Build_Tree(y, x);
+}
+
+void Tree::Store_matrix(int value, int i, int j, int col)
+{
+	arr[i*col+j] = value;
+}
+
+void Tree::add_Lnode(int value, int row, int col)
+{
+
+}
+void Tree::add_Rnode(int value, int row, int col)
+{
+
+}
+void Tree::add_Unode(int value, int row, int col)
+{
+
+}
+void Tree::add_Dnode(int value, int row, int col)
+{
+
+}
+int Tree::minPathSum()
+{
+
+}
+void Tree::Print(int row, int col)
+{
+
+}
